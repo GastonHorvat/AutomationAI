bf7dcd2228877f90cf3d61029a2655c0298a53b8
Gas <gastonhorvat@gmail.com>
2025-04-09 14:30:51 -0300

    Initial commit 2 to build the site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  output: 'static',
+  base: '/AutomationAI/', // importante si el repo no es del tipo "usuario.github.io"
 });
