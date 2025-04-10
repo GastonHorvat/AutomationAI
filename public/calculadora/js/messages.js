f80e0f9f26949edde054251bd65161e556920bdf
Gas <gastonhorvat@gmail.com>
2025-04-10 00:46:59 -0300

    Add standalone calculator in /public/calculadora

---
+++ public/calculadora/js/messages.js
@@ -0,0 +1,12 @@
+// Archivo de mensajes
+const MESSAGES = {
+    success: "Operation completed successfully!",
+    successColor: "#1da202",
+    error: "There was an error completing the operation.",
+    errorColor: "#ff002a",
+    emailSuccess: "Email sent successfully!",
+    emailError: "Error sending email: ",
+    validationError: "Please fill out all required fields.",
+    snackbarConsoleError: "The 'snackbar' element was not found in the DOM.",
+    // Agrega más mensajes según tus necesidades
+};
