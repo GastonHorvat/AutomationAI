97ed0cf81c2a03afd856bdd51c898f2fb98d6415
Gas <gastonhorvat@gmail.com>
2025-04-15 01:33:46 -0300

    Blogs 100% desde AirTable

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,16 +6,19 @@ import icon from "astro-icon"; // Importa la integración
 // https://astro.build/config
 export default defineConfig({
   output: 'static', // Correcto para GitHub Pages
+  
+  // Define las variables de entorno que necesitamos
   vite: {
     define: {
       'import.meta.env.AIRTABLE_API_KEY': JSON.stringify(process.env.AIRTABLE_API_KEY),
       'import.meta.env.AIRTABLE_BASE_ID': JSON.stringify(process.env.AIRTABLE_BASE_ID),
       'import.meta.env.AIRTABLE_TABLE_ID': JSON.stringify(process.env.AIRTABLE_TABLE_ID),
+      'import.meta.env.AIRTABLE_BLOG_TABLE_ID': JSON.stringify(process.env.AIRTABLE_BLOG_TABLE_ID),
     },
   },
+  
   // --- Propiedad 'site' para tu dominio personalizado ---
   site: 'https://automationai.solutions',
-
 
   // --- Integraciones ---
   integrations: [
