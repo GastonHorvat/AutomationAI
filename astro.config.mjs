72ec77b2aa6528167273a7b2f7900d0c15e9d13e
Gas <gastonhorvat@gmail.com>
2025-04-27 13:27:47 -0300

    Fix Categories

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,16 +7,6 @@ import icon from "astro-icon"; // Importa la integración
 export default defineConfig({
   output: 'static', // Correcto para GitHub Pages
   
-  // Define las variables de entorno que necesitamos
-  vite: {
-    define: {
-      'import.meta.env.AIRTABLE_API_KEY': JSON.stringify(process.env.AIRTABLE_API_KEY),
-      'import.meta.env.AIRTABLE_BASE_ID': JSON.stringify(process.env.AIRTABLE_BASE_ID),
-      'import.meta.env.AIRTABLE_TABLE_ID': JSON.stringify(process.env.AIRTABLE_TABLE_ID),
-      'import.meta.env.AIRTABLE_BLOG_TABLE_ID': JSON.stringify(process.env.AIRTABLE_BLOG_TABLE_ID),
-    },
-  },
-  
   // --- Propiedad 'site' para tu dominio personalizado ---
   site: 'https://automationai.solutions',
 
